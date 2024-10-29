cf951b7eeec96fd16dd2540597cb1159a6133baf
Andrew Domingues <contato@drewdomi.com>
2024-10-29 09:18:45 -0300

    chore: setup fastify

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log('hello')
+import Fastify from 'fastify'
+
+const app = Fastify()
+
+app.get('/', (_req, reply) => {
+  console.log('hello')
+  reply.send({ hello: 'world' })
+})
+
+app.listen({ port: 3000 }, (err, _addr) => {
+  if (err) throw err
+})
